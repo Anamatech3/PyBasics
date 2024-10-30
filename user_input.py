279217af2bf73e62f70f6a09fdff992d1ccdf126
Anamatech3 <dreynaarts3@gmail.com>
2024-10-31 00:57:04 +0300

    Your commit message

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please key in your name:")
+age = input("Please enter your age:")
+location = input("Please key in your location:")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
